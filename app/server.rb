e84f0da50c3e5619df5e4f4d4a87beddca74f535
Nic Yeeles <n.yeeles@gmail.com>
2014-06-20 12:51:07 +0100

    RSpec setup

--- app/server.rb
+++ app/server.rb
@@ -1,1 +1,12 @@
 require 'sinatra'
+
+env = ["RACK_ENV"] || "development"
+DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+
+require './models/peep'
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+get '/' do
+	"hello world"
+end
