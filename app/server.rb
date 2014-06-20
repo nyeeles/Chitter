9c66d458081acafc1eb29fd3920a0349d4923f38
Nic Yeeles <n.yeeles@gmail.com>
2014-06-20 16:56:22 +0100

    Slight layout and design tweaks to sign up form

--- app/server.rb
+++ app/server.rb
@@ -5,6 +5,7 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
 require_relative 'models/peep'
+require_relative 'models/user'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
---
+++ spec/features/user_management_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature "User signs up" do
+	
+	scenario "when being logged out" do
+		expect{ sign_up }.to change(User, :count).by 1
+		expect(User.first.email).to eq "test@test.com"
+	end
+
+	def sign_up(username = 'dave',
+							email = 'dave@test.com',
+							password = 'password')
+		visit '/sign-up'
+		expect(page.status_code).to eq 200
+		fill_in :username, with: username
+		fill_in :email, with: email
+		fill_in :password, with: password
+		click_button "Sign up"
+	end
+end
