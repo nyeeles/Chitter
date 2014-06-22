9fe10a94cebd1fc6fb1c6257ff96e58f0a9e67d2
Nic Yeeles <n.yeeles@gmail.com>
2014-06-22 23:47:00 +0100

    Users now have a full names field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User
 	include DataMapper::Resource
 
 	property :id, Serial
+	property :name, String
 	property :username, String
 	property :email, String
 	property :password_digest, Text
-	
+
 
 	def password=(password)
 		self.password_digest = BCrypt::Password.create(password)
